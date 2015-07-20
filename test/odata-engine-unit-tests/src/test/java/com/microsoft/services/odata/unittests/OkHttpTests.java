d2dfd3f7a6566365fe009bfc4845cbf0ac110bcb
Marcos Torres <v-marcto@microsoft.com>
2015-07-20 15:01:03 -0300

    WIP removing wiremock tests

---
+++ test/odata-engine-unit-tests/src/test/java/com/microsoft/services/odata/unittests/OkHttpTests.java
@@ -0,0 +1,61 @@
+package com.microsoft.services.odata.unittests;
+
+import com.microsoft.sampleservice.SampleEntity;
+import com.microsoft.sampleservice.fetchers.SampleContainerClient;
+import com.microsoft.services.orc.core.DependencyResolver;
+import com.microsoft.services.orc.log.LogLevel;
+import com.microsoft.services.orc.resolvers.OkHttpDependencyResolver;
+import com.squareup.okhttp.mockwebserver.MockWebServer;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class OkHttpTests {
+    private String url = "http://localhost:8080";
+    private SampleContainerClient client;
+    private DependencyResolver resolver;
+
+
+    public OkHttpTests() {
+
+    }
+
+    @Test
+    public void canRunTests() throws Exception {
+
+        resolver = new OkHttpDependencyResolver("footoken");
+        client = new SampleContainerClient(url, resolver);
+
+
+        MockWebServer server = new MockWebServer();
+        server.start();
+
+        Integer result = null;
+
+        try {
+            result = client.getMe()
+                    .getOperations()
+                    .twoParamsActionsFirstIsEntityType(getSampleEntity(), false)
+                    .get();
+
+        } catch (Throwable t) {
+            resolver.getLogger().log(t.getLocalizedMessage(), LogLevel.ERROR);
+        }
+
+        assertThat(result, is(notNullValue()));
+
+    }
+
+    private SampleEntity getSampleEntity() {
+        SampleEntity sampleEntity = new SampleEntity();
+        sampleEntity.setDisplayName("Some Display Name");
+        sampleEntity.setEntityKey("Some Entity Key");
+        sampleEntity.setId("5C338D75-CB90-4785-8667-CED25B3695BF");
+        return sampleEntity;
+    }
+
+
+}
